c2b352456b90dc8e2834dd22632aafbb679ef29b
DuongFPT <dobinhduong1705@gmail.com>
2024-09-04 18:46:30 +0700

    update

--- src/interface/order.ts
+++ src/interface/order.ts
@@ -1,4 +1,5 @@
 export interface Order{
+    _id: string;
     user_id?: string 
     name: string
     email: string
@@ -7,6 +8,7 @@ export interface Order{
     total_amount?: number
     type_payment?: number
     order_status?: number
+    order_date?: Date
 
     
 }
