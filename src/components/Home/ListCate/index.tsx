7ee86eb86f61dae458db33f36e0e14547fa1e25d
DuongFPT <dobinhduong1705@gmail.com>
2024-09-04 16:36:08 +0700

    update code

--- src/components/Home/ListCate/index.tsx
+++ src/components/Home/ListCate/index.tsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { getCategoriesHome } from "../../../data/Categories"
 import { Category } from "../../../interface/category"
-const link = import.meta.env.VITE_API_IMAGES
 
 const ListCateHome = () => {
     const [cateHome,setCateHome] = useState<Category[]>([])
@@ -26,7 +25,6 @@ const ListCateHome = () => {
         <Link to={`/products/${cate._id}`}>
         <h2 className="product-item_type">{cate.name}</h2> 
         <p className="product-text">{cate.mota}</p>
-        <img src={`${link}/${cate.img}`} alt=""/>
         <span>Xem tất cả</span>
       </Link>
     </li>
